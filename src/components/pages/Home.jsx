8b8c5f5148260ff5bde9862b35f346904b38cae1
Adele <adele@Adeles-MacBook-Pro.local>
2024-03-25 11:53:39 +0200

    home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,49 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+
+margin-top: 50px;
+   > h1{
+    text-align: center;
+   }
+   .description{
+    display: flex;
+    justify-content: center;
+    > p{
+        width: 500px;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-weight: bold;
+        font-style: italic;
+        line-height:30px;
+    }
+   }
+   > .button{
+    display: flex;
+    justify-content: center;
+    > a{
+        >button{
+            background-color: #116DFF;
+            color: white;
+            padding: 10px;
+        }
+    }
+   }
+`;
+
 const Home = () => {
     return ( 
-        <section>
-            <h1>Home Page</h1>
-        </section>
+        <StyledDiv>
+            <h1>Your Go-To Platform for Dialogue</h1>
+            <div className='description'>
+                <p>At our forum, we offer a vibrant online community where individuals from diverse backgrounds come together to exchange ideas, share experiences, and engage in meaningful discussions. Whether you're seeking advice, looking to connect with like-minded individuals, or simply exploring new perspectives, our platform provides a welcoming space for all. </p>
+            </div>
+            <div className='button'>
+               <Link to="forum/questions">
+                   <button>Go to FORUM</button>
+               </Link>
+            </div>
+        </StyledDiv>
      );
 }
  
