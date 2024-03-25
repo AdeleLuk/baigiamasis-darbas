b8542bd8a9ab2009ecc548851e0c28c24495d060
Adele <adele@Adeles-MacBook-Pro.local>
2024-03-25 09:23:19 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
+import Home from './components/pages/Home';
+import Forum from './components/pages/Forum';
+import Header from './components/UI/Header';
 
 const App = () => {
   return (
    <>
-
+     <Header />
+     <main>
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path='/forum'>
+            <Route path='questions' element={<Forum />}/>
+          </Route>
+          <Route path='/users'>
+              <Route path="login" element={<Login />}/>
+              <Route path="register" element={<Register />}/>
+          </Route>
+        </Routes>
+     </main>
    </>
   );
 }
--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,8 +1,74 @@
+import {Link, NavLink} from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+
+    background-color: #116DFF;
+    
+    >div {
+        height: 100px;
+        display: flex;
+        align-items: center;
+        > img{
+        height: 100%;
+     }
+     > nav{
+         > ul{
+            display: flex;
+            gap:50px;
+            list-style-type: none;
+             > li{
+                a {
+                    text-decoration: none;
+                    color: white;
+                    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+                }
+             }
+         }
+     }
+    }
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    > div:nth-child(3){
+        margin-right: 20px;
+    }
+    
+ 
+`;
+
 const Header = () => {
     return ( 
-        <section>
-            <h1>Header Page</h1>
-        </section>
+        <StyledHeader>
+            <div>
+                <img src="https://static.wixstatic.com/media/1aa858_8e4e2c2a9a7c496b8fd8da46f15b4782~mv2.jpg" alt="forum logo"/>
+            </div>
+            <div>
+              <nav>
+                <ul>
+                    <li>
+                        <NavLink to='/'>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='forum/questions'>Questions</NavLink>
+                    </li>
+                </ul>
+              </nav>
+            </div>
+            <div>
+              <nav>
+                <ul>
+                    <li>
+                        <NavLink to='users/login'>Log In</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='users/register'>Register</NavLink>
+                    </li>
+                </ul>
+              </nav>
+            </div>
+        </StyledHeader>
      );
 }
  
